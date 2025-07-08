b5995d5c7401bbd4a2aaa3f4ed4a5178ea0abb59
Pval-k <pravalhikak@gmail.com>
2025-07-08 16:08:13 -0700

    rotateArray

---
+++ arrays_strings/medium/rotateArray/rotateArray.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class rotateArray {
+    public void rotate(int[] nums, int k) {
+        //considers situation where k is greater than array length
+        k = k % nums.length;
+        int[] rotated = new int[nums.length];
+
+        for(int i = 0; i < nums.length; i++){
+            rotated[(i+k) % nums.length] = nums[i];
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = rotated[i];
+        }
+    }
+    public static void main(String[] args) {
+        rotateArray solution = new rotateArray();
+
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        solution.rotate(nums, k);
+
+        System.out.println("Rotated array: " + Arrays.toString(nums));  
+        // Output should be: [5, 6, 7, 1, 2, 3, 4]
+    }
+}
